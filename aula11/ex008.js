6ebf2ed7f0039d4966ef57403cf16edca544fac5
smateusx <smateux10@gmail.com>
2025-07-07 13:12:49 -0300

    finalizei aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 6
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+if (vel > 60) { //condição simples
+    console.log(`Você foi multado`)
+}
+console.log(`Dirija sempre usando o cinto de segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Espanha'
+if (país == 'Brasil') {
+    console.log(`Brasileiro, você nasceu no ${país}!`)
+} else {
+    console.log(`Estrangeiro, voce nasceu no ${país}!`)
+}
