48b282e8ceed4b55cad7926e3e6589e1e1a507e5
smateusx <smateux10@gmail.com>
2025-07-08 17:53:03 -0300

    finalizei ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Hello Word!')
+function calcular() {
+    let num = document.getElementById('num')
+    let tabuada = document.getElementById('tabuada')
+    if (num.value.length == 0){
+        alert('Digite um número!')
+    } else {
+        let n = Number(num.value)
+        tabuada.innerHTML = ''
+        for (let c=1; c<=10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tabuada${c}`
+            tabuada.appendChild(item)
+        }
+    }
+
+
+}
